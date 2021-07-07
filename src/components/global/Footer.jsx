9cc37840e3811f07dab6cf347831cdcf873b1668
Cameron Bulock <cameron@bulock.com>
2021-07-07 08:15:24 -0400

    improved footer style

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -2,12 +2,8 @@ import styled from 'styled-components';
 
 const SContent = styled.p`
 	margin: 4px auto 0;
-	background-color: ${({ theme }) => theme.secondary};
-	color: ${({ theme }) => theme.offWhite};
 	width: fit-content;
-	font-weight: 500;
 	padding: 0.75em;
-	border-radius: 8px 8px 0 0;
 `;
 
 const currentYear = new Date().getFullYear();
