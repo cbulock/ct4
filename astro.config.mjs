1f1d6c0f2c75a6692f229c294d1512f113b25721
Zeno <288338471+zeno-codebot@users.noreply.github.com>
2026-06-22 02:52:36 +0000

    Use Astro unified markdown processor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import { unified } from '@astrojs/markdown-remark';
 import remarkBreaks from 'remark-breaks';
 import { rehypeCindorCodeBlocks } from './src/lib/astro-markdown/rehypeCindorCodeBlocks.mjs';
 import { remarkLegacyContainers } from './src/lib/astro-markdown/remarkLegacyContainers.mjs';
@@ -7,7 +8,9 @@ export default defineConfig({
 	output: 'static',
 	markdown: {
 		syntaxHighlight: false,
-		remarkPlugins: [remarkBreaks, remarkLegacyContainers],
-		rehypePlugins: [rehypeCindorCodeBlocks],
+		processor: unified({
+			remarkPlugins: [remarkBreaks, remarkLegacyContainers],
+			rehypePlugins: [rehypeCindorCodeBlocks],
+		}),
 	},
 });
