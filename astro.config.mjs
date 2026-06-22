89e529b751d409d63fdfa92f0656dd6d3ca59279
Zeno <zeno@bulock.com>
2026-06-22 13:53:16 -0400

    Fix Astro markdown plugin configuration (#49)
    Co-authored-by: Zeno <288338471+zeno-codebot@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from 'astro/config';
-import { unified } from '@astrojs/markdown-remark';
-import remarkBreaks from 'remark-breaks';
 import { rehypeCindorCodeBlocks } from './src/lib/astro-markdown/rehypeCindorCodeBlocks.mjs';
 import { remarkLegacyContainers } from './src/lib/astro-markdown/remarkLegacyContainers.mjs';
 
@@ -8,9 +6,7 @@ export default defineConfig({
 	output: 'static',
 	markdown: {
 		syntaxHighlight: false,
-		processor: unified({
-			remarkPlugins: [remarkBreaks, remarkLegacyContainers],
-			rehypePlugins: [rehypeCindorCodeBlocks],
-		}),
+		remarkPlugins: [remarkLegacyContainers],
+		rehypePlugins: [rehypeCindorCodeBlocks],
 	},
 });
